fa414fc103c2c6643a2b46d97223e057f86fb3b5
Caleb Bray <cjbray23@gmail.com>
2018-08-16 14:59:21 -0700

    top content added with basic styles

--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -25,7 +25,7 @@
 	<?php endif; ?>
 
 	<div class="panel-content">
-		<div class="wrap">
+		<div class="not-wrap">
 			<header class="entry-header">
 				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
